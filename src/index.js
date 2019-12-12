b0a9e6814cff39f098b616d8c400e599e90be379
Jeremy <jeremy.bertin76@hotmail.fr>
2019-12-12 12:46:08 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WriteSmth = (props) => {
+    return <h1>{props.wrod1}</h1>
+}
+
+const WriteSmthElse = (props) => {
+    return <WriteSmth wrod1={props.word2}/>
+}
+
+const WriteAgainSmth = (props) => {
+    return <WriteSmthElse word2={props.word3}/>
+}
+
+ReactDOM.render(<WriteAgainSmth word3="Hello" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
